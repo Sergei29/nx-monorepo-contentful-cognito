247ed5aab42dcf13fd6b9d805da86b7c4e2d5a9c
Sergei29 <34629784+Sergei29@users.noreply.github.com>
2024-03-29 17:58:31 +0000

    feat: apps express server (#36)
    * chore: nx/express plugin installed
    
    * feat: apps express server added
    
    * fix: unit test and e2e test updated

---
+++ apps/nx-pubsub-server/__tests__/demo.test.ts
@@ -0,0 +1,6 @@
+// this is the demo file - TOBE removed once unit tests are added.
+describe('Demo test', () => {
+  it('should run the test demo file', () => {
+    expect(1).toEqual(1);
+  });
+});
---
+++ apps/nx-pubsub-server/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'nx-pubsub-server',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/nx-pubsub-server',
+};
